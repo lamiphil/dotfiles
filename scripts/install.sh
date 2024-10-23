4a3c8f508c517705316f1f4c91e4533bbc9b4acd
Philippe Lamy <philippelamy98@outlook.com>
2024-10-22 21:09:41 -0400

    Ajout du script d'installation des packages apt

---
+++ scripts/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Vérifier si le script est exécuté entant que root ou avec sudo
+if ["$EUID" -ne 0]; then
+  echo "ERREUR - Exécuter ce script entant que root ou avec sudo."
+  exit 1
+fi
+
+# Liste des packages à installer
+PACKAGES=(
+  "git"
+  "neovim"
+  "tree"
+  "python3-pip"
+  "lsd"
+  "bat"
+)
+
+echo "Mise à jour de la liste des packages..."
+apt update -y
+
+echo "Installation des packages..."
+for package in "${PACKAGES[@]}"; do 
+  apt install "$package" -y 
+done
+
+echo "Ménage..."
+apt autoremove -y
+apt clean
+
+echo "Les packages ont été installés avec succès !"
