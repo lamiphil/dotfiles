7128af2436cf0341f41cd2dfabb1f358914b78a3
Philippe Lamy <philippelamy98@outlook.com>
2024-10-24 10:40:50 -0400

    Switch les mappings pour toggle et créer nouveau terminal

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -7,4 +7,23 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Switch toggable and new terminal mappings
+-- toggleable
+map("n", "<leader>h", function()
+  require("nvchad.term").toggle { pos = "sp" }
+end, { desc = "terminal toggleable horizontal term" })
+
+map("n", "<leader>v", function()
+  require("nvchad.term").toggle { pos = "vsp" }
+end, { desc = "terminal toggleable vertical term" })
+
+-- new
+map({ "n", "t" }, "<A-v>", function()
+  require("nvchad.term").new { pos = "vsp", id = "vtoggleTerm" }
+end, { desc = "terminal new vertical term" })
+
+map({ "n", "t" }, "<A-h>", function()
+  require("nvchad.term").new { pos = "sp", id = "htoggleTerm" }
+end, { desc = "terminal new horizontal term" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
