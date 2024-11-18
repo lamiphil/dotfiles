a8bddb209c192b22332914e5004cdcb9a2ada70e
Philippe Lamy <philippelamy98@outlook.com>
2024-11-18 14:31:57 -0500

    Correction script create_symlinks

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-# Définit les dossiers de configurations sources
-REPO_HOME_DIR="/home/code/perso/dotfiles/home"
-REPO_CONFIG_DIR="/home/code/perso/dotfiles/config"
+# Détecter automatiquement l'emplacement de ce script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Définir les dossiers de configurations sources en fonction de l'emplacement du script
+REPO_HOME_DIR="$(realpath "$SCRIPT_DIR/../home")"
+REPO_CONFIG_DIR="$(realpath "$SCRIPT_DIR/../config")"
+
+echo $REPO_HOME_DIR
+echo $REPO_CONFIG_DIR
 
 # Définir les dossiers de destination
-SYSTEM_HOME_DIR="/home"
-SYSTEM_CONFIG_DIR="/home/config"
+SYSTEM_HOME_DIR="$HOME"
+SYSTEM_CONFIG_DIR="$HOME/.config"
+echo $SYSTEM_HOME_DIR
+echo $SYSTEM_CONFIG_DIR
 
 # Vérifier si --dry-run est utilisé
 DRY_RUN=false
 if [[ "$1" == "--dry-run" ]]; then
   DRY_RUN=true
-  echo "---- Dry run activée. Aucun changement seront effectués. ----"
+  echo -e "---- Dry run activée. Aucun changement seront effectués. ----"
 fi
 
 # Fonction qui permet de créer les symlinks
@@ -29,18 +37,27 @@ create_symlink() {
 }
 
 # Créer les symlinks pour tous les fichiers dans /home
-echo "Traitement du dossier $REPO_HOME_DIR vers $SYSTEM_HOME_DIR..."
-for item in "$REPO_HOME_DIR"/*; do
+echo -e "\nTraitement du dossier $REPO_HOME_DIR vers $SYSTEM_HOME_DIR..."
+for item in "$REPO_HOME_DIR"/{*,.*}; do
+  # Ignorer les dossiers `.` et `..` et les motifs non résolus
+  if [[ ! -e "$item" || $(basename "$item") == "." || $(basename "$item") == ".." ]]; then
+    continue
+  fi
+
   # Extraire le nom du fichier ou du dossier
   base_item=$(basename "$item")
-
   # Créer le symlink
   create_symlink "$item" "$SYSTEM_HOME_DIR/$base_item"
 done
 
 # Créer les symlinks pour tous les fichiers dans /config
-echo "Traitement du dossier $REPO_CONFIG_DIR vers $SYSTEM_CONFIG_DIR..."
-for item in "$REPO_CONFIG_DIR"/*; do
+echo -e "\nTraitement du dossier $REPO_CONFIG_DIR vers $SYSTEM_CONFIG_DIR..."
+for item in "$REPO_CONFIG_DIR"/{*,.*}; do
+  # Ignorer les dossiers `.` et `..`
+  if [[ ! -e "$item" || $(basename "$item") == "." || $(basename "$item") == ".." ]]; then
+    continue
+  fi
+
   # Extraire le nom du fichier ou du dossier
   base_item=$(basename "$item")
 
@@ -53,4 +70,4 @@ for item in "$REPO_CONFIG_DIR"/*; do
   create_symlink "$item" "$SYSTEM_CONFIG_DIR/$base_item"
 done
 
-echo "Les symlinks ont été créés !"
+echo -e "\n---- Les symlinks ont été créés ! ----"
