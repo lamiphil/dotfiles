df7fef22d290a4fe457ee00e38fce34d601561cd
Philippe Lamy <philippelamy98@outlook.com>
2025-01-05 17:54:31 -0500

    NVIM - Ajout mappings Dismiss Noice Message

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -5,14 +5,19 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- Vim Tmux Navigator
+------------------------
+-- Vim Tmux Navigator -- 
+------------------------
 map("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
 map("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
 map("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
 map("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true })
 
--- Switch toggleable and new terminal mappings
--- toggleable
+-------------------------------------------------
+-- Switch toggleable and new terminal mappings --
+-------------------------------------------------
+
+-- toggleable -- 
 map("n", "<leader>h", function()
   require("nvchad.term").toggle { pos = "sp" }
 end, { desc = "terminal toggleable horizontal term" })
@@ -21,7 +26,7 @@ map("n", "<leader>v", function()
   require("nvchad.term").toggle { pos = "vsp" }
 end, { desc = "terminal toggleable vertical term" })
 
--- new
+-- new --
 map({ "n", "t" }, "<A-v>", function()
   require("nvchad.term").new { pos = "vsp", id = "vtoggleTerm" }
 end, { desc = "terminal new vertical term" })
@@ -31,3 +36,5 @@ map({ "n", "t" }, "<A-h>", function()
 end, { desc = "terminal new horizontal term" })
 
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", {desc = "Dismiss Noice Message"})
