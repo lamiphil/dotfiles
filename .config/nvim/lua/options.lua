e1ffbf6e5e7c8a3684e052f8b2961e60aedbd2ab
Philippe Lamy <philippelamy98@outlook.com>
2025-02-04 14:08:15 -0500

    NVIM - Ajout highlight yank

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -12,4 +12,14 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- Highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
+  end,
+})
+
 
