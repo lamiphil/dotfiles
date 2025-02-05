49cbc23a9f7f2e14dc071a86b6384cde8f624a21
Philippe Lamy <philippelamy98@outlook.com>
2025-02-05 15:52:11 -0500

    ENV - Ajout locale FR UTF8 et script pour l'installer

---
+++ scripts/configure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Vérifier si le script est exécuté entant que root ou avec sudo
+if ["$EUID" -ne 0]; then
+  echo "ERREUR - Exécuter ce script entant que root ou avec sudo."
+  exit 1
+fi
+
+# Configurer la locale FR
+locale-gen fr_FR.UTF-8
