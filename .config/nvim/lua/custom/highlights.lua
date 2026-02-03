4ba65eb35fb2462b79d11f1c12ca3230ce272ea1
Philippe Lamy <philippelamy98@outlook.com>
2026-02-03 16:13:38 -0500

    NVIM - Removed the highlights for nvimTree

--- .config/nvim/lua/custom/highlights.lua
+++ .config/nvim/lua/custom/highlights.lua
@@ -19,7 +19,7 @@ print(colors.colors1)
 M.add = vim.tbl_extend("force", {
 
   -- Exemples de groupes intégrés à Neovim
-  Normal       = { fg = colors.color7 or "#c0c3c1", bg = colors.color0 or "#061008" },
+  Normal       = { fg = colors.color7 or "#c0c3c1", bg = "NONE" },
   LineNr       = { fg = colors.color8 or "#566959" },
   Comment      = { fg = colors.color5 or "#0BAFD4", italic = true },
   Visual       = { bg = colors.color2 or "#02AE9A" },
@@ -36,34 +36,6 @@ M.add = vim.tbl_extend("force", {
   MarkviewPalette4 = { fg = "#a6e3a1", bg = "#3c4948", bold = true },
   MarkviewPalette5 = { fg = "#74c7ec", bg = "#314358", bold = true },
   MarkviewPalette6 = { fg = "#b4befe", bg = "#3c405b", bold = true },
-
-  -- Git status couleurs NvimTree
-  NvimTreeGitDeleted = { fg = "#e06c75" },
-  NvimTreeGitDirty = { fg = "#ff9e64" },
-  NvimTreeGitIgnored = { fg = "#6f737b" },
-  NvimTreeGitMerge = { fg = "#d19a66" },
-  NvimTreeGitNew = { fg = "#ff9e64" },
-  NvimTreeGitRenamed = { fg = "#e5c07b" },
-  NvimTreeGitStaged = { fg = "#98c379" },
-
-  -- Liens pour Git
-  NvimTreeGitFileDeletedHL = { link = "NvimTreeGitDeleted" },
-  NvimTreeGitFileDirtyHL = { link = "NvimTreeGitDirty" },
-  NvimTreeGitFileIgnoredHL = { link = "NvimTreeGitIgnored" },
-  NvimTreeGitFileMergeHL = { link = "NvimTreeGitMerge" },
-  NvimTreeGitFileNewHL = { link = "NvimTreeGitNew" },
-  NvimTreeGitFileRenamedHL = { link = "NvimTreeGitRenamed" },
-  NvimTreeGitFileStagedHL = { link = "NvimTreeGitStaged" },
-
-  NvimTreeGitFolderDeletedHL = { link = "NvimTreeGitDeleted" },
-  NvimTreeGitFolderDirtyHL = { link = "NvimTreeGitDirty" },
-  NvimTreeGitFolderIgnoredHL = { link = "NvimTreeGitIgnored" },
-  NvimTreeGitFolderMergeHL = { link = "NvimTreeGitMerge" },
-  NvimTreeGitFolderNewHL = { link = "NvimTreeGitNew" },
-  NvimTreeGitFolderRenamedHL = { link = "NvimTreeGitRenamed" },
-  NvimTreeGitFolderStagedHL = { link = "NvimTreeGitStaged" },
-
-  NvimTreeGitStagedIcon = { link = "NvimTreeGitFileStagedHL" }
 
 })
 
