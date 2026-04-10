369b0559be8fe923bd6dbcbd45da45c50666cb30
Philippe Lamy <philippelamy98@outlook.com>
2026-04-09 22:31:17 -0400

    TV - Use tab for cycling sources

--- .config/nvim/lua/configs/tv.lua
+++ .config/nvim/lua/configs/tv.lua
@@ -19,8 +19,8 @@ return {
           keybinding = "<C-f>",
           handlers = {
             ["<CR>"] = h.open_as_files,
+            ["<C-h>"] = h.open_in_split,
             ["<C-q>"] = h.send_to_quickfix,
-            ["<C-s>"] = h.open_in_split,
             ["<C-v>"] = h.open_in_vsplit,
             ["<C-y>"] = h.copy_to_clipboard,
           },
@@ -30,8 +30,8 @@ return {
           keybinding = "<leader><leader>",
           handlers = {
             ["<CR>"] = h.open_at_line,
+            ["<C-h>"] = h.open_in_split,
             ["<C-q>"] = h.send_to_quickfix,
-            ["<C-s>"] = h.open_in_split,
             ["<C-v>"] = h.open_in_vsplit,
             ["<C-y>"] = h.copy_to_clipboard,
           },
